70cee7219ef89930a9a7ec7140825b8c8fedff25
adryel97 <adryel.giovaniny@gmail.com>
2022-06-15 17:20:12 -0300

    criando o template

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+    <title>VisionCar</title>
+</head>
+<body>
+    <?php require './view/templates/header.php' ?>
+    <?php require './view/templates/navbar.php' ?>
+    <main class="content"></main>
+    <?php require './view/templates/footer.php' ?>
+</body>
+</html>
---
+++ view/templates/footer.php
@@ -0,0 +1,4 @@
+<script src="node_modules/jquery/dist/jquery.min.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+<script src="node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
