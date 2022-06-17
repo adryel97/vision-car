4994c88022eeba5c38e30abf3339af7aa424fc0a
adryel97 <adryel.giovaniny@gmail.com>
2022-06-17 10:49:52 -0300

    alterando template index

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>VisionCar</title>
     <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css">
     <link rel="stylesheet" href="node_modules/remixicon/fonts/remixicon.css">
 </head>
 <body>    
-    <div class="container">
+    <div class="container position-relative">
         <?php require './view/templates/header.php' ?>
         <?php require './view/templates/navbar.php' ?>
         <main class="content">
