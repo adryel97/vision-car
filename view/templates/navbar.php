c3dfc96e29a4b1052f5e1821f219d6a652b1c853
adryel97 <adryel.giovaniny@gmail.com>
2022-06-17 14:39:36 -0300

    alteracao na navbar

--- view/templates/navbar.php
+++ view/templates/navbar.php
@@ -2,25 +2,34 @@
     <div class="nav__br--daddy">
         <ul class="navbar-nav d-flex flex-column mb-3 ul__nav" href="#">
             <li class="nav-item mb-2">
-                <span href="#" class="d-flex flex-row flex-nowrap">
-                    <i class="ri-home-2-fill center-icon text-gray"></i>
-                    <span class="list_description ms-2 text-gray fw-semibold">Home</span>
+                <span href="#" class="d-flex flex-row flex-nowrap align-items-center">
+                    <i class="ri-dashboard-2-fill center-icon text-gray icon__navbar"></i>
+                    <span class="list_description ms-2 text-gray fw-semibold">Dashboard</span>
                 </span>
             </li>
             <li class="nav-item">
-                <a href="#" class="d-flex flex-row flex-nowrap collapse__nav" data-collapse="#teste">
-                    <i class="ri-home-2-fill center-icon text-gray"></i>
-                    <span class="list_description ms-2 text-gray fw-semibold">Home</span>
+                <a href="#" class="d-flex flex-row flex-nowrap collapse__nav align-items-center" data-collapse="#teste">
+                    <i class="ri-coins-fill center-icon text-gray icon__navbar"></i>
+                    <span class="list_description ms-2 text-gray fw-semibold">Financeiro</span>
                 </a>
-                <ul class="list__collapse" id="teste" style="display: none;">
-                    <li>
-                        <a href="" class="list_description">link</a>
+                <ul class="list__collapse mt-2" id="teste" style="display: none;">
+                    <li class="mb-2">
+                        <a href="" class="list_description text-gray" style="font-size: 0.9rem">Contas</a>
                     </li>
-                    <li>
-                        <a href="" class="list_description">link</a>
+                    <li class="mb-2">
+                        <a href="" class="list_description text-gray" style="font-size: 0.9rem">Contas a pagar</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="" class="list_description text-gray" style="font-size: 0.9rem">Contas a pagas</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="" class="list_description text-gray" style="font-size: 0.9rem">Fluxo de caixa</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="" class="list_description text-gray" style="font-size: 0.9rem">Lançamentos</a>
                     </li>
                 </ul>
             </li>
-        </ul>    
+        </ul>       
     </div>
 </nav>
