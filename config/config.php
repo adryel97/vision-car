6c91624f0eeaf89f441bee65c4ed4bd92cd35c5c
adryel97 <108001234+criacao3litoralcar@users.noreply.github.com>
2022-07-29 15:44:55 -0300

    configurado para conectar com o banco de dados

--- config/config.php
+++ config/config.php
@@ -1,7 +1,22 @@
 <?php
+session_start();
 setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
 date_default_timezone_set('America/Sao_Paulo');
 define('ROOT', 'http://'.$_SERVER['SERVER_NAME']);
+define('DATA_LAYER_CONFIG', [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'port' => '3306',
+    'dbname' => 'db_visioncar',
+    'username' => 'root',
+    'passwd' => '',
+    'options' => [
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_CASE => PDO::CASE_NATURAL
+    ]
+]);
 function url(string $uri = null): string
 {
     if ($uri) {
