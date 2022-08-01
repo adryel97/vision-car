ab3648e4e69505c9559eb07e4748e865d155ed99
adryel97 <108001234+criacao3litoralcar@users.noreply.github.com>
2022-08-01 14:03:16 -0300

    insert favicon

--- view/templates/_template.php
+++ view/templates/_template.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="<?=url()?>/img/logo/visioncar_mini.png"  type="image/png" sizes="15x15" />
     <link rel="stylesheet" href="<?=url()?>/css/bootstrap.css">
     <link rel="stylesheet" href="<?=url()?>/css/style.css?v<?=time()?>">
     <title><?=$title?></title>
