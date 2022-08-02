6ccca9b8db01406384326383ea1c16e88e4a295b
adryel97 <108001234+criacao3litoralcar@users.noreply.github.com>
2022-08-02 11:06:23 -0300

    animation in dropdown

--- view/templates/header.php
+++ view/templates/header.php
@@ -12,7 +12,7 @@
                         <p class="m-0 fw-semibold">Usuário <span><i class="ri-arrow-drop-down-line center-icon" style="font-size: 1.3rem"></i></span></p>
                         <p class="text-gray m-0" style="font-size: 0.80rem">Loja 2</p>
                     </div>
-                    <ul class="dropdown-menu border-0 shadow">
+                    <ul class="dropdown-menu border-0 shadow animate__bounceIn animate__animated animate__faster mt-5">
                         <li><a class="dropdown-item" href="#"><i class="ri-user-settings-line center-icon" style="font-size: 1.0rem"></i> Perfil</a></li>
                         <li><a class="dropdown-item" href="<?=$router->route('logout.action')?>"><i class="ri-shut-down-line center-icon" style="font-size: 1.0rem"></i> Sair</a></li>
                     </ul>
